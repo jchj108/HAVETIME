d097dbfdcd8c81683f04c64fc608a2f2bc0bd3c7
jchj108 <jchj108@naver.com>
2022-05-05 15:02:58 +0900

    jpa 등록 jpa 테스트 메소드 실행 application.properties 에서 로그에 sql 포함하도록 변경

---
+++ src/main/java/com/ch/freetime/com/ch/freetime/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.ch.freetime.com.ch.freetime.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(length = 500, nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/ch/freetime/com/ch/freetime/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.ch.freetime.com.ch.freetime.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+
+}
